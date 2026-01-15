0d4728d2d680bc7ad30163e704059e7404cde60a
luxKn0x <cristianstefan95@yahoo.com>
2026-01-15 10:54:17 +0200

    Delete src directory

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-public class EcommerceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
-    }
-}
